7de395ab3209af453e9d68b009ef8923858c23dd
daniel14311531 <daniel14311531@163.com>
2026-02-09 20:39:44 +0800

    feat: add config and notebook for shakespeare training
    Adds a new training configuration for the Shakespeare character dataset
    tailored for the DeltaNet model. The configuration is set up for quick
    debugging runs on CPU or smaller hardware.
    
    Additionally, this commit updates `model_list.py` to support dynamic loading
    of model configurations via a new `get_config` helper function. A Jupyter
    notebook is included to streamline the setup and execution of the training
    process. The `.gitignore` file is updated to exclude the `.github` directory.

---
+++ config/train_shakespeare_char_lin_attn.py
@@ -0,0 +1,88 @@
+# train a miniature shakespeare model
+# good for debugging and playing on macbooks and such
+from utils.config import *
+from models import *
+from model_list import get_config
+
+model_name = 'deltanet' # [TODO]
+model_config = get_config(model_name)
+
+# I/O
+io_config = IOConfig(
+    out_dir = 'out-shakespeare-char',
+    eval_interval = 250, # keep frequent because we'll overfit
+    eval_iters = 200,
+    log_interval = 10, # don't print too too often
+    always_save_checkpoint = False, # we expect to overfit on this small dataset, so only save when val improves
+    init_from = 'scratch',
+)
+
+# wandb
+wandb_config = WandbConfig(
+    wandb_log= False,
+    wandb_project= 'shakespeare-char',
+    wandb_run_name= model_name, # [TODO]
+)
+
+# data
+data_config = DataConfig(
+    dataset = 'shakespeare_char',
+    gradient_accumulation_steps = 1,
+    batch_size = 64,
+    block_size = 256, # context of up to 256 previous characters
+)
+
+# baby GPT model :)
+model_config_instance = model_config(
+    model_name = model_name,
+    n_layer = 6,
+    n_head = 6,
+    n_embd = 384,
+    dropout = 0.2,
+    block_size = 256,
+    bias = False,
+
+    conv_size=4,
+    initial_state=False,
+    eta=1,
+)
+
+# optimizer
+optimizer_config = OptimizerConfig(
+    learning_rate = 5e-4, # with baby networks can afford to go a bit higher
+    max_iters = 5000,
+    lr_decay_iters = 5000, # make equal to max_iters usually
+    min_lr = 1e-4, # learning_rate / 10 usually
+    beta2 = 0.99, # make a bit bigger because number of tokens per iter is small
+    warmup_iters = 100, # not super necessary potentially
+)
+
+# system (cpu)
+system_config = SystemConfig(
+    backend = 'gloo',  # gloo backend works better for small data
+    device = 'cpu',  # run on cpu only
+    compile = True # do not torch compile the model
+)
+# system (cuda)
+# system_config = SystemConfig(
+#     backend = 'nccl',  # gloo backend works better for small data
+#     device = 'cuda',  # run on cpu only
+#     compile = False # do not torch compile the model
+# )
+
+# config = {
+#     "io": asdict(io_config),
+#     "wandb": asdict(wandb_config),
+#     "data": asdict(data_config),
+#     "model": asdict(model_config_instance),
+#     "optimizer": asdict(optimizer_config),
+#     "system": asdict(system_config),
+# }
+config = {
+    "io": io_config,
+    "wandb": wandb_config,
+    "data": data_config,
+    "model": model_config_instance,
+    "optimizer": optimizer_config,
+    "system": system_config,
+}
